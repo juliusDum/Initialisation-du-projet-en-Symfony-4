0cc31e6c9da85edde8d2938706e3d814e957f8f6
Jules DUMAS <jules_ox@hotmail.fr>
2020-06-02 16:22:33 +0200

    symfony validation

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -6,9 +6,12 @@ use App\Repository\ProgramRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=ProgramRepository::class)
+ * @UniqueEntity("title", message="cette serie existe déjà")
  */
 class Program
 {
@@ -21,11 +24,15 @@ class Program
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="je ne dois pas être vide")
+     * @Assert\Length(max="255", maxMessage="La catégorie saisie {{ value }} est trop longue, elle ne doit pas dépasser {{ limit }} caractères")
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank(message="je ne dois pas être vide")
+     * @Assert\Regex(pattern="/plus belle la vie/", match=false, message="On parle de vraies séries ici")
      */
     private $summary;
 
